3892317633bb7aa24fc5aa8c0eed2342735a1595
JINJU PARK <aricclunj@naver.com>
2025-05-01 01:31:10 +0900

    .

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react';
 // API 요청을 쉽게 해주는 axios 라이브러리
 import axios from 'axios';
 
+import PokemonCard from './PokemonCard'
+
 // 포켓몬 개체 하나를 위한 타입 정의 (name과 url이 들어있는 객체)
 interface Pokemon {
   name: string; // 포켓몬 이름
@@ -17,10 +19,11 @@ const Pokedex: React.FC = () => {
   // 컴포넌트가 화면에 처음 보여질 때 딱 한 번 실행되는 코드 (useEffect 사용)
   useEffect(() => {
     // axios로 API 호출해서 포켓몬 151마리 목록 불러오기
-    axios.get('https://pokeapi.co/api/v2/pokemon?limit=151')
-      .then((response) => {
+    axios
+      .get('https://pokeapi.co/api/v2/pokemon?limit=30') // 포켓몬 30마리만 가져오기
+      .then((res) => {
         // 받아온 포켓몬 데이터를 상태에 저장 (setPokemonList 실행)
-        setPokemonList(response.data.results);
+        setPokemonList(res.data.results);
       })
       .catch((error) => {
         // 에러가 나면 콘솔에 출력하기
@@ -29,26 +32,23 @@ const Pokedex: React.FC = () => {
   }, []); // 의존성 배열이 빈 배열이면 -> 처음 1번만 실행이란 뜻
 
   return (
-    <div>
+    <>
       <h1>포켓몬 도감</h1>
       
       {/* 포켓몬 목록을 하나씩 돌면서 이름 출력 */}
-      <ul>
-      {pokemonList.map((pokemon, index) => {
-          const id = index + 1; // 포켓몬 ID는 1부터 시작
-          const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
-
-          return (
-            <li key={pokemon.name}>
-              <img src={imageUrl} alt={pokemon.name} />
-              <span>{pokemon.name}</span>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
-};
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {pokemonList.map((pokemon, index) => (
+          <PokemonCard
+            key={index}
+            name={pokemon.name}
+            id={index + 1}
+            image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`}
+          />
+        ))}
+      </div>
+    </>
+  )
+}
 
 // 이 컴포넌트를 외부에서도 쓸 수 있게 내보냄
 export default Pokedex;
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,55 +1,38 @@
 // React 기본 기능
-import React, { useState } from 'react';
-import './PokemonCard.css';
+import React, { useState } from "react";
+import "./PokemonCard.css";
 
 interface PokemonCardProps {
-  name: string;
-  imageUrl: string;
-  id: number;
-  koreanName: string;
-  height: number;
-  weight: number;
-  types: string[];
-  abilities: string[];
-  moves: string[];
+  name: string; // 포켓몬 이름
+  image: string; // 포켓몬 이미지 주소
+  id: number; // 포켓몬 ID (상세 API 호출에 사용)
 }
 
-// 함수형 컴포넌트 정의
-const PokemonCard: React.FC<PokemonCardProps> = ({
-  name, imageUrl, id, koreanName, height, weight, types, abilities, moves
-}) => {
-  // 카드가 뒤집혔는지를 저장하는 상태값
+const PokemonCard: React.FC<PokemonCardProps> = ({ name, image, id }) => {
+  // 클릭하면 뒤집히는 상태값 정의 (false면 앞면)
   const [flipped, setFlipped] = useState(false);
 
-  // 클릭했을 때 뒤집힘 상태를 반전시키는 함수
+  // flipped / 클릭하면 상태를 반전시켜줌줌
   const handleClick = () => {
     setFlipped(!flipped);
   };
 
   return (
-    // 카드 전체 영역에 클릭 이벤트와 클래스명 지정
-    <div className={`card ${flipped ? 'flipped' : ''}`} onClick={handleClick}>
-      {/* 앞면 - 포켓몬 이름과 이미지 */}
+    // 카드 클릭 시 상태 변경
+    <div
+      className={`pokemon-card ${flipped ? "flipped" : ""}`}
+      onClick={handleClick}
+    >
+      {/* 앞면 */}
       <div className="card-front">
-        <img src={imageUrl} alt={name} />
+        <img src={image} alt={name} />
         <p>{name}</p>
       </div>
 
-      {/* 뒷면 - 정보들 */}
+      {/* 뒷면: 현재는 임시 텍스트. 추후에 포켓몬 상세 정보 넣을 예정! */}
       <div className="card-back">
-        <h2>{koreanName} (#{id})</h2>
-        <img src={imageUrl} alt={name} />
-        <p>English Name: {name}</p>
-        <p>Height: {height}</p>
-        <p>Weight: {weight}</p>
-        <p>Type: {types.join(', ')}</p>
-        <p>Abilities: {abilities.join(', ')}</p>
-        <p>Moves:</p>
-        <ul>
-          {moves.slice(0, 7).map((move, index) => (
-            <li key={index}>{move}</li>
-          ))}
-        </ul>
+        <p>포켓몬 정보 출력 예정</p>
+        <p>키라던지 몸무게라던지 특성같은거</p>
       </div>
     </div>
   );
