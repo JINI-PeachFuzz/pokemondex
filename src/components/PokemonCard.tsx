12495482c42112d2f039aee43335a8dfad2ec088
JINJU PARK <aricclunj@naver.com>
2025-05-03 22:08:02 +0900

    .

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -57,6 +57,12 @@ const PokemonCard: React.FC<PokemonCardProps> = ({ name, image, id }) => {
   }, [id]);
 
   // 카드가 클릭되어 뒤집히면 상세 정보 API 호출 (뒤집었을 때만 실행)
+  // flipped는 카드가 클릭되어 뒤집혔는지 상태를 저장하는 값이고 detail은 카드가 뒤집혔을 때 한번만 API를 불러와 상세 정보를 저장함
+  
+  /* 1. 카드앞면에서 이미지와 한글이름이 없으면 로딩텍스트나오게
+  2. 카드클릭시 flipped상태를 반전시켜 앞/뒷면을 전환시킴
+  3. 카드가 flipped상태일땐 API를 통해 상세정보 키, 몸무게,타입등 을 가져오게
+  4. 타입은 translateType함수를 통해 한글로 변환시킴 */
   useEffect(() => {
     if (flipped && !detail) {
       axios
