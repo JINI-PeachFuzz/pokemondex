c91084508031950f07fc5b1381f0ecc5974ddb93
JINJU PARK <aricclunj@naver.com>
2025-05-11 23:43:00 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,57 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import HeaderTabs from "./components/Header";
 import PokedexPage from "./pages/PokedexPage";
 import ShadowGamePage from "./pages/ShadowGamePage";
 import Footer from "./components/Footer";
 import './App.css';
 
-function App() {
+function AppWrapper() {
   return (
     <Router>
-      <div className="app-container">
-        <HeaderTabs />
-        <div className="main-content">
-          <Routes>
-            <Route path="/pokedex" element={<PokedexPage />} />
-            <Route path="/shadowgame" element={<ShadowGamePage />} />
-          </Routes>
-        </div>
-        <Footer />
-      </div>
+      <AppWithBackground />
     </Router>
   );
 }
 
-export default App;
+function AppWithBackground() {
+  const location = useLocation();
+
+  return (
+    <div className="app-container">
+      <HeaderTabs />
+
+      <div className="main-content">
+        <Routes>
+          <Route
+            path="/pokedex"
+            element={
+              <div className="bg-pokedex">
+                <PokedexPage />
+              </div>
+            }
+          />
+          <Route
+            path="/shadowgame"
+            element={
+              <div className="bg-shadowgame">
+                <ShadowGamePage />
+              </div>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <div className="bg-main">
+                <div>메인 페이지</div>
+              </div>
+            }
+          />
+        </Routes>
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default AppWrapper;
