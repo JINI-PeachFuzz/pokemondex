518044360b610144bb1bd8c20887d8a2f1129944
JINJU PARK <aricclunj@naver.com>
2025-05-13 18:43:50 +0900

    .

--- src/pages/PokedexPage.tsx
+++ src/pages/PokedexPage.tsx
@@ -4,7 +4,7 @@ import Pokedex from "../components/Pokemon";
 const PokedexPage: React.FC = () => {
   const [search, setSearch] = useState(""); 
   const [searchInput, setSearchInput] = useState(""); 
-  const [sort, setSort] = useState("number-asc"); // 정렬 기준
+  const [sort, setSort] = useState("number-asc"); 
 
   const handleSearchSubmit = () => {
     setSearch(searchInput); 
