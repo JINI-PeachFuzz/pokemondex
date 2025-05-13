f5a04d180b94b9a608e0a34d8ddd42521805cb1a
JINJU PARK <aricclunj@naver.com>
2025-05-13 18:42:21 +0900

    .

--- src/pages/PokedexPage.tsx
+++ src/pages/PokedexPage.tsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import Pokedex from "../components/Pokemon";
 
 const PokedexPage: React.FC = () => {
-  const [search, setSearch] = useState(""); // 실제 검색에 사용되는 값
-  const [searchInput, setSearchInput] = useState(""); // 검색창 값
-  const [sort, setSort] = useState("number-asc"); // 정렬 기준
+  const [search, setSearch] = useState(""); 
+  const [searchInput, setSearchInput] = useState(""); 
+  const [sort, setSort] = useState("number-asc"); 
 
   const handleSearchSubmit = () => {
     setSearch(searchInput); 
