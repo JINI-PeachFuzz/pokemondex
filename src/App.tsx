e1aa372f9a6d6e13a292919ad2ced4bc498cc209
JINJU PARK <aricclunj@naver.com>
2025-05-13 19:03:29 +0900

    ,

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import HeaderTabs from "./components/Header";
 import PokedexPage from "./pages/PokedexPage";
 import ShadowGamePage from "./pages/ShadowGamePage";
